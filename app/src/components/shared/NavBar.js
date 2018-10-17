bb263511aea52d88ac4b77c91d3497592509137c
Trace <tcarrasco@wisc.edu>
2018-10-17 16:26:02 -0500

    Merge conflic resolved

--- app/src/components/shared/NavBar.js
+++ app/src/components/shared/NavBar.js
@@ -1,14 +1,2 @@
-// Trace has edit this 1
+//Test
 // Merge conflict?
-
-
-// Trace has edit this 2
-
-
-
-// Trace has edit this 3
-
-// Trace has edit this 4
-// Trace has edit this 5
-// Trace has edit this 6
-
