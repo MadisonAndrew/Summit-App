53cbbee1737d75da90fc9fffa43f162ac352955e
Trace <tcarrasco@wisc.edu>
2018-10-17 16:10:49 -0500

    some updates to NavBar

--- app/src/components/shared/NavBar.js
+++ app/src/components/shared/NavBar.js
@@ -1,2 +1,15 @@
 //Test
+
+// Trace has edit this 1
 // Merge conflict?
+
+
+// Trace has edit this 2
+
+
+
+// Trace has edit this 3
+
+// Trace has edit this 4
+// Trace has edit this 5
+// Trace has edit this 6
