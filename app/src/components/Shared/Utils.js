455cbcd6beca484e707c4a9c8553b00d21950479
MadisonAndrew <andrewpelto@gmail.com>
2018-11-15 22:56:48 -0600

    ClientRegistration -Updated the client registration to do error checking on the names. The date does not require it because you are unable to enter an invalid date. -Implemented a date selector that is iOS friendly, allowing the users to simply scroll
    ClientRegistrationVirtualController
    -Added the controller which calls the AddUser Firebase function. This will insert a new user and
    returns the query result to verify the call went through successfully.
    
    Utils
    -Added a name function which should be used consistently across the application to verify the name entered
    is valid (letters and spaces)
    -Added a function to print the date

--- app/src/components/Shared/Utils.js
+++ app/src/components/Shared/Utils.js
@@ -23,4 +23,32 @@ var queryBuilder = (function () {
     return queryBuilder;
 }());
 
-export {queryBuilder};
+var printDate = function(date) {
+    if(date === null)
+        return "";
+    // +1 because month is 0 indexed
+    return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+}
+
+var verifyName = function(name) {
+    if(name === null)
+        return "";
+    else{
+        //Remove Trailing and Leading whitespace from name
+        var result = name.trim();
+        //Check for invalid chars
+        var lettersAndSpacesOnly = /^[A-Za-z\s]+$/; 
+        if(lettersAndSpacesOnly.test(result)){
+            return result;
+        }
+        else{
+            //alert('Found invalid chars')
+            return "";
+        }
+    }
+        
+}
+
+export {
+    queryBuilder, verifyName, printDate,
+};
