c8ffc995f7adc7ab4916380e3797d4cca5f4e686
Chen Yang <chen@pporn.tv>
2018-11-17 22:17:04 -0600

    add CreateAlertVirtualController

--- app/src/components/CreateAlert/CreateAlertContainer.jsx
+++ app/src/components/CreateAlert/CreateAlertContainer.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-
+import { createAlert } from './CreateAlertVirtualController';
 
 class CreateAlertContainer extends Component {
     constructor() {
         super();
+
+        // state info
         this.state = {
             name: '',
-            datesEffective: '',
-            timeToLive: '',
-            description: ''
+            dates_effective: '',
+            ttl: '',
+            description: '',
+            is_created: false,
         }
+
+        // bind all functions
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onFinishAlertCreation = this.onFinishAlertCreation.bind(this);
     };
-
 
     handleChange({ target }) {
         this.setState({
@@ -24,6 +29,20 @@ class CreateAlertContainer extends Component {
     handleSubmit (event) {
         event.preventDefault();
         console.log(this.state);
+        createAlert(this.state, this.onFinishAlertCreation);
+    }
+
+    onFinishAlertCreation(isSuccessful) {
+        if(isSuccessful) {
+            // change stae
+            this.setState({
+                is_created: true,
+            });
+
+            console.log('created');
+        } else {
+            console.log('failed to create alert');
+        }
     }
 
     render() {
@@ -41,7 +60,7 @@ class CreateAlertContainer extends Component {
                 <h3>Enter dates effective:</h3>
                 <input
                     type="text"
-                    name="datesEffective"
+                    name="dates_effective"
                     placeholder="Enter dates effective:"
                     value={ this.state.datesEffective }
                     onChange={ this.handleChange }
@@ -50,7 +69,7 @@ class CreateAlertContainer extends Component {
                 <h3>Enter time to live:</h3>
                 <input
                     type="text"
-                    name="timeToLive"
+                    name="ttl"
                     placeholder="Enter time to live:"
                     value={ this.state.timeToLive }
                     onChange={ this.handleChange }
@@ -66,6 +85,10 @@ class CreateAlertContainer extends Component {
                 />
 
                 <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+
+                {this.state.is_created &&
+                    <h3>Alert Created</h3>
+                }
             </div>
         );
     }
