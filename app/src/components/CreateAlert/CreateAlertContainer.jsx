80268f453534c4e8cb7150c3da907b55d2297952
Chen Yang <chen@pporn.tv>
2018-11-17 20:22:07 -0600

    fix bugs in CreateAlertContainer.jsx

--- app/src/components/CreateAlert/CreateAlertContainer.jsx
+++ app/src/components/CreateAlert/CreateAlertContainer.jsx
@@ -2,75 +2,72 @@ import React, { Component } from 'react';
 
 
 class CreateAlertContainer extends Component {
-  constructor() {
-  super();
-  this.state = {
-    name: null,
-    datesEffective: null,
-    timeToLive: null,
-    description: null
-  }
-  this.handleChange = this.handleChange.bind(this);
-};
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            datesEffective: '',
+            timeToLive: '',
+            description: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
 
 
-handleChange({ target }) {
-    this.setState({
-      [target.name]: target.value
-    });
-  }
+    handleChange({ target }) {
+        this.setState({
+            [target.name]: target.value
+        });
+    }
 
-  handleSubmit (event) {
-      alert('here')
-      event.preventDefault()
-      console.log(this.state)
-      this.setState({
-          firstName: '',
-          lastName: '',
-          dateOfBirth: ''
-      })
-  }
+    handleSubmit (event) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return(
-          <div>
-            <h3>Enter name of alert:</h3>
-              <input
-                   type="text"
-                   name="Name"
-                   placeholder="Enter name of alert:"
-                   value={ this.state.name }
-                   onChange={ this.handleChange }
-                 />
-             <h3>Enter dates effective:</h3>
-               <input
+            <div>
+                <h3>Enter name of alert:</h3>
+                <input
                     type="text"
-                    name="Date"
+                    name="name"
+                    placeholder="Enter name of alert:"
+                    value={ this.state.name }
+                    onChange={ this.handleChange }
+                />
+
+                <h3>Enter dates effective:</h3>
+                <input
+                    type="text"
+                    name="datesEffective"
                     placeholder="Enter dates effective:"
                     value={ this.state.datesEffective }
                     onChange={ this.handleChange }
-                  />
-            <h3>Enter time to live:</h3>
-              <input
-                   type="text"
-                   name="Time"
-                   placeholder="Enter time to live:"
-                   value={ this.state.timeToLive }
-                   onChange={ this.handleChange }
-                 />
-             <h3>Enter description:</h3>
-               <input
+                />
+
+                <h3>Enter time to live:</h3>
+                <input
                     type="text"
-                    name="Description"
-                    placeholder="Enter description:"
-                    value={ this.state.Description }
+                    name="timeToLive"
+                    placeholder="Enter time to live:"
+                    value={ this.state.timeToLive }
                     onChange={ this.handleChange }
-                  />
-            <input type="submit" value="Submit"/>
+                />
 
-    </div>
+                <h3>Enter description:</h3>
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Enter description:"
+                    value={ this.state.description }
+                    onChange={ this.handleChange }
+                />
 
-        )
-
+                <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+            </div>
+        );
     }
 }
 
